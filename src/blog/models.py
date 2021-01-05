c59343c3d4a3e98527d2f598000712c6da0cb6b2
SemihDurmus <semihdurmus@yahoo.com>
2021-01-06 00:36:02 +0100

    Media root

--- src/blog/models.py
+++ src/blog/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     OPTIONS = (
@@ -16,10 +19,13 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to="")
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=OPTIONS, default='d')
     slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return self.title
