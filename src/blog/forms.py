0d4ae41ba63a0d6ba99ba362287464257604dd9a
SemihDurmus <semihdurmus@yahoo.com>
2021-01-06 10:32:18 +0100

    Add forms: PostForm, CommentForm

---
+++ src/blog/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models Post, Comment, Category
+
+
+class PostForm(forms.ModelForm):
+    status = forms.ChoiceField(choices=Post.OPTIONS)
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(), empty_label="Select")
+
+    class Meta:
+        model = Post
+        fields = (
+            'title', 'image', 'content', 'category', 'status'
+        )
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content', )
