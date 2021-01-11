f5bd6ce906613170e02782c516d54365d9376cef
SemihDurmus <semihdurmus@yahoo.com>
2021-01-11 14:05:39 +0100

    Profile

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,16 +1,32 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class RegistrationForm(UserCreationForm):
+
+    email = forms.EmailField()  # to make email a required field
+
     class Meta:
         model = User
         # password and password confirmation will be inherited from UserCreationForm
         fields = ("username", "email")
 
-    def clean_email(self):
+    def clean_email(self):  # to make each email unique
         email = self.cleaned_data["email"]
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("This email is already taken")
         return email
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ("image", "bio")
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("username", "email")
--- src/users/views.py
+++ src/users/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render, redirect
-from .forms import RegistrationForm
+from .forms import RegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -12,3 +12,18 @@ def register(request):
         "form": form
     }
     return render(request, "users/register.html", context)
+
+
+def profile(request):
+    u_form = UserUpdateForm(request.POST or None, instance=request.user)
+    p_form = ProfileUpdateForm(
+        request.POST or None, request.FILES or None, instance=request.user.profile)
+    if u_form.is_valid() and p_form.is_valid():
+        u_form.save()
+        p_form.save()
+        return redirect(request.path)
+    context = {
+        "u_form": u_form,
+        "p_form": p_form,
+    }
+    return render(request, "users/profile.html", context)
