212424b762ea2f650020fc4ff068f7146570bd46
SemihDurmus <semihdurmus@yahoo.com>
2021-01-12 00:56:11 +0100

    Prevent registering while logged + messages

--- src/users/views.py
+++ src/users/views.py
@@ -1,12 +1,18 @@
 
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
 
 
 def register(request):
     form = RegistrationForm(request.POST or None)
+    if request.user.is_authenticated:
+        messages.warning(request, "You already have an account")
+        return redirect("blog:list")
     if form.is_valid():
         form.save()
+        name = form.cleaned_data["username"]
+        messages.success(request, f"Account created for {name}")
         return redirect("login")
     context = {
         "form": form
@@ -21,6 +27,7 @@ def profile(request):
     if u_form.is_valid() and p_form.is_valid():
         u_form.save()
         p_form.save()
+        messages.success(request, "Your profile has been updated")
         return redirect(request.path)
     context = {
         "u_form": u_form,
