812d8214b140582446b7dd700df760590db4b288
SemihDurmus <semihdurmus@yahoo.com>
2021-01-11 12:30:49 +0100

    Register

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import register
 
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(), name="Login")
+    path("register/", register, name="register")
+    #path("login/", auth_views.LoginView.as_view(), name="Login")
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+
+
+def register(request):
+    form = RegistrationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        # return redirect("login")
+    context = {
+        "form": form
+    }
+    return render(request, "users/register.html", context)
